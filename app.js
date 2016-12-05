f64b7ad276bce18985df9bcdf48201744d99e8ee
Eduardo <eduardo.gouveia9090@gmail.com>
2016-12-04 23:14:16 -0200

    Desperate Publish

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var app     = express();
 var path    = require("path");
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/views/product.html'));
-  //__dirname : It will resolve to your project folder.
+  //res.sendFile(path.join(__dirname+'/views/product.html'));
+  res.sendFile(path.join(__dirname+'/build/product.html'));
 });
 
 app.use(express.static(__dirname + '/images'));
@@ -16,6 +16,8 @@ app.use(express.static(__dirname + '/chico'));
 app.use(express.static(__dirname + '/scripts'));
 
 app.use(express.static(__dirname + '/styles'));
+
+app.use(express.static(__dirname + '/build'));
 
 app.listen(3000, function(){
     console.log("Running at Port 3000...Press CTRL+C to stop.");
